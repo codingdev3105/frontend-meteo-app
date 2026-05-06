cf32cd6609f5153c4755e7253cb316ff0061d457
codingdev3105 <yahiahanani2001@gmail.com>
2026-05-06 18:10:09 +0200

    Update API_URL to Vercel backend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://backend-meteo-app.vercel.app/api';
 
 // Instance avec Intercepteur pour le Token
 const instance = axios.create({
